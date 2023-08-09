c4744b896c8c5ecc2ac69dfe082de5e23593d74a
Chetan Patil <C8709040124@GMAIL.COM>
2023-08-09 18:56:42 +0530

    removed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
-import logo from "./Resources/acp_logo.png";
 import "./App.css";
-import { Navbar } from "react-bootstrap";
-import HomeNavbar from './Components/HomePageNavbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Component } from "react";
-import HomePageNavbar from "./Components/HomePageNavbar";
-
+import Header from "./Components/Header";
+import HomeContent from "./Components/HomeContent";
+import SideBar from "./Components/SideBar";
 // import NavScrollExample from "./Navbar.js";
 
 function App() {
   return (
-     <div className="App">
-      
-      <HomePageNavbar />
-      
-      <header className="App-header">
-      
-        <p className="app_title"> <b>Welcome to ACPLAY</b></p>
-        {/* <a
-          className="App-link"
-          href="http://localhost:3000/" 
-          // target="_blank"
-          rel="noopener noreferrer"
-        >
-          Click here to Refresh the Page
-        </a> */}
-      </header>
-
+    <div>
+      <Header />
+      <HomeContent />
+      <SideBar />
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div className="h-14 bg-[#310D3E]">
+           <input className=' placeholder:p-4 bg-[#310D3E] placeholder:text-yellow-500 border border-[#ca3efd] ' type='text' placeholder='Search' name='search' />
+            <a href="">
+                <button></button>
+            </a>
+        </div>
+    </div>
+  )
+}
+
+export default Header
